bf0104540546a9c76aa9a9498c8fd823c18d47df
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-14 16:12:03 +0300

    add all array methods

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
 const items = [
-    { name: 'Bike', price: 100},
-    { name: 'TV', price: 200},
-    { name: 'Album', price: 10},
-    { name: 'Book', price: 5},
-    { name: 'Phone', price: 500},
-    { name: 'Computer', price: 1000},
-    { name: 'Keyboard', price: 25},
+    { name: 'Bike',    price: 100},
+    { name: 'TV',    price: 200},
+    { name: 'Album',    price: 10},
+    { name: 'Book',    price: 5},
+    { name: 'Phone',    price: 500},
+    { name: 'Computer',    price: 1000},
+    { name: 'Keyboard',    price: 25}
 ]
 
-// Filter method
+// Filter Method
 
-
-const filteredItems = items.filter((item)=> {
-    return item.price <= 100
+const filteredItems = items.filter((item) => {
+    return item.price <=100
 })
+console.log(filteredItems);
 
-console.log(filteredItems)
+// Map Method
 
-// Map method
-
-const itemsPrice = items.map((item) =>{
-    return item.price
+const itemNames = items.map((item) => {
+    return item.name
 })
+console.log(itemNames);
 
-console.log(itemsPrice)
-
-// Find method
+//Find Method
 
 const foundItem = items.find((item) => {
-    return item.name === 'Book'
+    return item.name === 'Computer'
 })
+console.log(foundItem);
 
-console.log(foundItem)
+//For Each
 
-
-// For each method
-
-items.forEach((item)=>{
+items.forEach((item) => {
     console.log(item.price)
 })
 
+//Some method
+
 const hasInexpensiveItems = items.some((item) => {
-    return item.price <= 10000
+    return item.price <= 100
 })
 
-console.log(hasInexpensiveItems)
+console.log(hasInexpensiveItems);
+
+//Every method
+
+const hasCheapItems = items.every((item) => {
+    return item.price <= 100
+})
+
+console.log(hasCheapItems);
+
+//Reduce method
+
+const total = items.reduce((currentTotal,item) => {
+    return item.price + currentTotal
+}, 0)
+
+console.log(total);
+
+//Includes
+
+const myArray = [1,2,3,4,5,6,7,8,9,10]
+
+const includesTwo = myArray.includes(2)
+
+console.log(includesTwo);
+
+
